5aea5b9b6641a02ab211005357afa88dd972f4f7
Enu0 <yinuoche@buffalo.edu>
2022-08-01 20:59:22 -0400

    fix defaultProps color for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,8 +18,8 @@ function Header({text, bgColor, textColor}) {
 }
 Header.defaultProps = {
     text: "Feedback UI",
-    bgColor: 'rbga(0,0,0,0.4)',
-    textColor: '#ff6e95'
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95'
 }
 Header.propTypes = {
     text: PropTypes.string,
