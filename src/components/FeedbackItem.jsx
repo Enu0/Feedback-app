ae0f647b78690a7026eba0cb543776b006e5b94e
Enu0 <yinuoche@buffalo.edu>
2022-08-02 01:59:17 -0400

    add a close icon

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,15 @@
 import Card from './shared/Card';
 import PropTypes from 'prop-types'
+import { FaTimes } from 'react-icons/fa';
 
 function FeedbackItem({item}) {
 
   return(
      <Card reverse = {false}>
         <div className='num-display'>{item.rating}</div>
+        <button className = 'close'>
+          <FaTimes color = 'black'/>
+        </button>
         <div className='text-display'>{item.text}</div>
     </ Card>
   )
